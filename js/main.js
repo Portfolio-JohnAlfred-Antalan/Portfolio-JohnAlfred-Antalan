3e58b1c0d4f4149c6307a58d7d7da890c8fda4b3
Portfolio-JohnAlfred-Antalan <ja.antalan2020@gmail.com>
2026-04-05 07:26:28 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -77,13 +77,15 @@ $(document).ready(function() {
 
   var typed = $(".typed");
 
-  $(function() {
-    typed.typed({
-      strings: ["John Alfred "],
-      typeSpeed: 70,
-      loop: true,
-    });
+$(function() {
+  typed.typed({
+    strings: ["Video Editor", "Videographer", "Director"],
+    typeSpeed: 70,
+    backSpeed: 40,      // Smoothly deletes the text
+    backDelay: 2000,    // Pause before switching to the next role
+    loop: true,
   });
+});
 
 
   // ========================================================================= //
